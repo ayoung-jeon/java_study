d67abe6526d7d47645f5e80802e43f2fa10ba5da
ayoung-jeon <wjsdkdud71@naver.com>
2023-10-25 13:24:33 +0900

    Chapter5
    연습문제풀이-young

---
+++ src/java/young_s/Chapter5/Exercise5_2.java
@@ -0,0 +1,21 @@
+package src.java.young_s.Chapter5;
+
+public class Exercise5_2 {
+    public static void main(String[] args) {
+        System.out.println("자바의 정석 연습풀이2");
+    }
+    // [5-2] 다음과 같은 배열이 있을 때, arr[3].length의 값은 얼마인가?
+
+    /*
+    int[][] arr = {
+                    { 5,5,5,5,5 },
+                    { 10, 10, 10 },
+                    { 20, 20, 20, 20 },
+                    { 30, 30 }
+                  };
+     */
+
+    // 답 -> 오류..?
+}
+
+
